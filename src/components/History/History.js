f3c5f227f57add34ca6401eac25f25b2a9ce9c52
Heini Carlberg <heini.carlberg@gmail.com>
2020-04-27 13:18:39 +0300

    History palautettu alkutekijöihin

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -3,32 +3,17 @@ import moment from 'moment';
 import Content from '../Content/Content';
 
 
-function History(props) {
-  
-  let kuukausi = moment(props.kuukausi);
+function History (props){
+
+
 
   return(
-    <Content>
+  <Content>
+    <div className="history"><h2>Edelliset kuukaudet</h2>
+    </div>
+      </Content>
 
-      <div className="history"><h2>Historia</h2>
-        <div className="history__row">
-          <div className="history__kuukausi">{kuukausi.format("MMMM.YYYY")}</div>
-        </div>
+  );
+  }
 
-        <div className="history__row">
-         <div className="history__kasvikset">{props.data.eurot}</div>
-         <div className="history__maitotuotteet">Maitotuotteet 50€</div>
-         </div>
-
-         <div className="history__row">
-          <div className="history__proteiinit">Proteiinit 90 €</div>
-          <div className="history__proteiinit">Viljat 20 €</div>
-        </div>
-      </div>
-        </Content>
-
-    );
-}
-
-   
  export default History;
