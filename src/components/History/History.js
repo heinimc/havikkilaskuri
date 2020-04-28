ba9e102c00327b68758c781f3ccfad2bc0cf314e
Heini Carlberg <heini.carlberg@gmail.com>
2020-04-28 13:23:37 +0300

    History-komponentti luotu

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import moment from 'moment';
 import Content from '../Content/Content';
+import './History.css';
 
 
 function History (props){
 
+  const reducer = (kkHävikki, nykyHävikki) => {
+  const index = kkHävikki.findIndex(item => item.päivä ===  moment(nykyHävikki.päivä).format('YYYY-M'));
 
-
-  return(
-  <Content>
-    <div className="history"><h2>Edelliset kuukaudet</h2>
-    </div>
-      </Content>
-
-  );
+    if (index >=0) {
+      kkHävikki[index].eurot = kkHävikki[index].eurot + nykyHävikki.eurot;
+      kkHävikki[index].grammat = kkHävikki[index].grammat + nykyHävikki.grammat;
+    } else {
+      kkHävikki.push({päivä: moment(nykyHävikki.päivä).format('YYYY-M'), eurot:nykyHävikki.eurot, grammat:nykyHävikki.grammat});
+    }
+    return kkHävikki;
   }
+
+ 
+ let kkHävikki = props.data.reduce(reducer, []);
+ let rows = kkHävikki.map(item =>
+  <div className="edeltäväkuukausi__row">
+    <div>{item.päivä}</div>
+    <div>{item.eurot} €</div>
+    <div>{item.grammat} g</div>
+  </div>
+ )
+
+ return(
+   
+  <Content>
+    <div className="edeltäväkuukausi"><h3>Edellisten kuukausien kokonaishävikki</h3>
+      {rows}
+     </div>
+  </Content>
+ 
+);
+}
 
  export default History;
